fe542e1258f6e072b968fbaeb5289d0350b34844
miXania <mbraverman96@gmail.com>
2014-07-15 11:35:33 +0200

    "Counting Stars" Commit (v1.2)
    - Rearranged the screen by moving the least important stuff to the
    bottom. ↕️
    - Changed the GUI a bit by placing two rectangles over the edges of the
    screen. 💻
    - Added an hour counter at the bottom of the screen that shows how long
    the program has been running. 🏇
    - The 24 hour records don’t display unless 12 hours have passed since
    they will be the same anyway. 📊

--- ThermoStar/ThermoStar.ino
+++ ThermoStar/ThermoStar.ino
@@ -1,5 +1,5 @@
 /*
- Thermo Star v1.1 "Beyond Perfection"
+ ThermoStar v1.2 "Counting Stars"
   
  Created 25 June 2014
  by Michael Braverman
@@ -46,8 +46,9 @@ String temperatureMinString[24];
 unsigned long previosMillis; // For screen and sensor refreshing
 unsigned long previosTimeMillis; // For counting one hour
 const long oneHour = 3600000;
+int hourCount;
 boolean refresh = true;
-const int RefreshInterval = 15000;
+const int refreshInterval = 15000; 
 
 void setup() {
   dht.begin(); 
@@ -80,7 +81,7 @@ void loop() {
   refresh = false; // Set to false until triggered again
   
   // If "previosMillis" milliSeconds pass 
-  if ((millis() - previosMillis) > RefreshInterval){
+  if ((millis() - previosMillis) > refreshInterval){
     previosMillis = millis();
     refresh = true;
   }
@@ -89,6 +90,7 @@ void loop() {
   if ((millis() - previosTimeMillis) > oneHour){
     previosTimeMillis = millis();
     shiftArray();
+    hourCount ++;
   }
   
   // If millis() overflows it will equal less than previosMillis 
@@ -124,50 +126,72 @@ void mainScreen(){
   TFTscreen.background(0, 0, 0);
   TFTscreen.setTextSize(2);
   TFTscreen.stroke(0, 233, 242);
-  TFTscreen.text("Temperature:", 5, 20);
+  TFTscreen.text("Temperature:", 10, 5);
   TFTscreen.setTextSize(5);
-  TFTscreen.text(temperaturePrintout, 12, 42);
+  TFTscreen.text(temperaturePrintout, 12, 25);
   TFTscreen.setTextSize(3);
-  TFTscreen.text("C", 132, 56);
+  TFTscreen.text("C", 132, 39);
   
   TFTscreen.setTextSize(1);
   TFTscreen.stroke(67, 175, 58);
-  TFTscreen.text("Humidity", 100, 92);
+  TFTscreen.text("Humidity", 100, 67);
   TFTscreen.setTextSize(3);
-  TFTscreen.text(humidityPrintout, 100, 105);
-  TFTscreen.text("%", 137, 105);
+  TFTscreen.text(humidityPrintout, 100, 80);
+  TFTscreen.text("%", 137, 80);
   
   // The graph lines and text
   TFTscreen.setTextSize(1);
+  TFTscreen.stroke(100, 100, 100);
+  TFTscreen.rect(0, 106, 160, 22); // Bottom rectangle (more dark)
   TFTscreen.stroke(255, 255, 255);
-  TFTscreen.text("Thermo Star v1.1", 5, 5);
-  TFTscreen.text("12h", 32, 92);
-  TFTscreen.text("24h", 63, 92);
-  TFTscreen.line(5, 101, 85, 101);
-  TFTscreen.line(5, 114, 85, 114);
-  TFTscreen.line(26, 92, 26, 127);
-  TFTscreen.line(57, 92, 57, 127);
+  TFTscreen.rect(0, 0, 160, 107);  // Top rectangle (more light)
+  TFTscreen.stroke(255, 255, 255);
+  TFTscreen.text("ThermoStar v1.2", 2, 119);
+  TFTscreen.text("12h", 32, 67);
+  TFTscreen.text("24h", 63, 67);
+  TFTscreen.line(5, 76, 85, 76);
+  TFTscreen.line(5, 89, 85, 89);
+  TFTscreen.line(26, 67, 26, 102);
+  TFTscreen.line(57, 67, 57, 102);
+  // To display the hourCount on the TFT screen
+  TFTscreen.stroke(253, 164, 252);
+  TFTscreen.text("Hours Up:", 2, 109); 
+  String(hourCount).toCharArray(temperaturePrintout, 5); 
+  TFTscreen.text(temperaturePrintout, 59, 109);
   
   // Numbers and names in the graph
   TFTscreen.stroke(0, 0, 255);
-  TFTscreen.text("Max", 5, 105);
+  TFTscreen.text("Max", 5, 80);
   // Find the hottest record in the past 12 hours (-1)
   temperatureMaxString[maxTemp(11)].toCharArray(temperaturePrintout, 5);
-  TFTscreen.text(temperaturePrintout, 29, 105);
-  // Find the hottest record in the past 24 hours (-1)
-  temperatureMaxString[maxTemp(23)].toCharArray(temperaturePrintout, 5);
-  TFTscreen.text(temperaturePrintout, 60, 105);
+  TFTscreen.text(temperaturePrintout, 29, 80);
+  
+  // Unless 12 hours have passed, don't display the 24 hours maximum record
+  if (hourCount < 12) { 
+    TFTscreen.text("-", 70, 80); // Instead show that not enought hours passed
+  } else {
+    // Find the hottest record in the past 24 hours (-1)
+    temperatureMaxString[maxTemp(23)].toCharArray(temperaturePrintout, 5);
+    TFTscreen.text(temperaturePrintout, 60, 80);
+  }
+  
   TFTscreen.stroke(255, 150, 0);
-  TFTscreen.text("Min", 5, 117);
+  TFTscreen.text("Min", 5, 92);
   // Find the coldest record in the past 12 hours (-1)
   temperatureMinString[minTemp(11)].toCharArray(temperaturePrintout, 5);
-  TFTscreen.text(temperaturePrintout, 29, 117);
-  // Find the coldest record in the past 24 hours (-1)
-  temperatureMinString[minTemp(23)].toCharArray(temperaturePrintout, 5);
-  TFTscreen.text(temperaturePrintout, 60, 117);
+  TFTscreen.text(temperaturePrintout, 29, 92);
+  
+  // Unless 12 hours have passed, don't display the 24 hours minimum record
+  if (hourCount < 12) { 
+    TFTscreen.text("-", 70, 92); // Instead show that not enought hours passed
+  } else {
+    // Find the coldest record in the past 24 hours (-1)
+    temperatureMinString[minTemp(23)].toCharArray(temperaturePrintout, 5);
+    TFTscreen.text(temperaturePrintout, 60, 92);
+  }
 }
 
-// Find the hottest record from the array
+// Find the hottest record in the array
 byte maxTemp(byte pastHours){
   float Max = temperatureValue;
   byte maxIndex;
@@ -180,7 +204,7 @@ byte maxTemp(byte pastHours){
   return maxIndex;
 }
 
-// Find the coldest record from the array
+// Find the coldest record in the array
 byte minTemp(byte pastHours){
   float Min = temperatureValue;
   byte minIndex;
