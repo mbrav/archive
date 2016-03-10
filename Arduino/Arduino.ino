4be12a76d65fa9aa668c2d40e18449f7860bd568
mixania <mbraverman96@gmail.com>
2016-03-09 20:49:29 -0500

    sending three ints..." tzt-tzt! "over.."

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,19 +1,21 @@
 // Created by Michael Braverman
 // 9 March 2016
 
-int switchPin = 4;                      
+int switchPin = 4;
+int x = 111;
+int y = 222;  
+int z = 333;                        
 
-void setup() {
-  pinMode(switchPin, INPUT);             
+void setup() {            
   Serial.begin(115200);                    
 }
 
 void loop() {
-  if (digitalRead(switchPin) == HIGH) {  
-    Serial.println(1);               
-  } else {                               
-    Serial.println(0);               
-  }
+                              
+  Serial.print(x);  
+  Serial.print(y); 
+  Serial.print(z);              
+
   delay(100);                            
 }
 
