e6e9c2f9c64b8d4f432cd4d2b7fce419edabed09
mixania <mbraverman96@gmail.com>
2016-03-17 16:49:05 -0400

    Reading MPU-6050 readings

---
+++ in.fORM.ino
@@ -0,0 +1,40 @@
+// Created by Michael Braverman
+// 9 March 2016
+
+// MPU-6050 sensor setup
+#include<Wire.h>
+const int address=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+
+void setup() {
+  Wire.begin();
+  Wire.beginTransmission(address);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // READ SENSOR
+  Wire.beginTransmission(address);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(address,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  // SEND TO SERIAL
+  Serial.print(',');
+  Serial.print(AcX);
+  Serial.print(',');
+  Serial.print(AcY);
+  Serial.print(',');
+  Serial.println(AcZ);
+}
