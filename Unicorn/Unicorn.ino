10c985f8636fa49e7eca6eb719595a7202ec05b3
mixania <mbraverman96@gmail.com>
2016-04-12 00:43:27 -0400

    Did u hear the Furious Unicron Thunder last night?
    One color which lights the whole strip in various colors for .5 second in a in a lightening
    effect (interpretative)

--- Unicorn/Unicorn.ino
+++ Unicorn/Unicorn.ino
@@ -34,7 +34,6 @@ void loop() {
 		hue = hue + 60;
 	}
 
-
 	// fadeUp();
 	// fadeDown();
 	// unicornPurpleRain();
@@ -45,20 +44,139 @@ void loop() {
 }
 
 void unicornThunder() {
-	int startSpark = random(0, (NUM_LEDS)-1);
-	boolean upOrDown = random(0,1);
+	// pick a random location where the thunder starts
+	unsigned int startSpark = random(0, (NUM_LEDS)-1);
+	// which direction way does the thunder move?
+	byte upOrDown = random(0,2);
+	// pick a color between deepBlue and blue-green
+	byte thunderColor = random(20,50);
+	// define thunder frequency
+	// time to wait until the next one in milleseconds
+	unsigned int thunderDelay= random(500, 3000);
 
-	leds[startSpark] = CHSV(30, 255, 255);
+	// fist spark
+	leds[startSpark] = CHSV(thunderColor, 255, 255);
 	FastLED.show();
 	delay(20);
-	leds[startSpark] = CHSV(30, 255, 10);
+	leds[startSpark] = CHSV(thunderColor, 255, 10);
 	FastLED.show();
 	delay(20);
-	leds[startSpark] = CHSV(30, 255, 255);
+	leds[startSpark] = CHSV(thunderColor, 255, 255);
 	FastLED.show();
 	delay(10);
 	leds[startSpark] = CRGB::Black;
-	
+
+	Serial.println(upOrDown);
+
+	// thunder strike up the led
+	if (upOrDown == 0) {
+		byte brigthtnessDecay = 0;
+		for (int i = startSpark; i < (NUM_LEDS)-1; i++ ) {
+			// fade Down
+			for (int j = 255; j > 150; j--) {
+				leds[i] = CHSV(thunderColor, 255, j - brigthtnessDecay );
+
+				// only show every 20 loops for faster timing
+				// light speed doesn't like big delays
+				if (j % 20 == 0) {
+					FastLED.show();
+					brigthtnessDecay++;
+				}
+			}
+			// fade Up next one
+			for (int j = 0; j < 255; j++) {
+				leds[i+1] = CHSV(thunderColor, 255, j - brigthtnessDecay);
+
+				// only show every 20 loops for faster timing
+				// light speed doesn't like big delays
+				if (j % 20 == 0) {
+					FastLED.show();
+					brigthtnessDecay++;
+				}
+			}
+
+			// set to black
+			leds[i] = CRGB::Black;
+			leds[i+1] = CRGB::Black;
+		}
+	}
+
+	// hunder strike down the led
+	else {
+		for (int i = startSpark; i >= 0; i-- ) {
+			// fade Down
+			for (int j = 255; j > 150; j--) {
+				leds[i] = CHSV(thunderColor, 255, j);
+
+				// only show every 20 loops for faster timing
+				// light speed doesn't like big delays
+				if (j % 20 == 0) {
+					FastLED.show();
+				}
+			}
+			// fade Up next one
+			for (int j = 0; j < 255; j++) {
+				leds[i-1] = CHSV(thunderColor, 255, j);
+
+				// only show every 20 loops for faster timing
+				// light speed doesn't like big delays
+				if (j % 20 == 0) {
+					FastLED.show();
+				}
+			}
+
+			// set to black
+			leds[i] = CRGB::Black;
+			leds[i-1] = CRGB::Black;
+		}
+	}
+
+	delay(50);
+	// The Sky After Glow, less dim
+
+	// if the thunder headed down
+	// draw the brigtheest LED at bottom
+	// dim the LED's as you go up
+	if (upOrDown == 0) {
+		// repeat flash 5 times
+		for (int i = 0; i < 5; i++) {
+			// glow flash on
+			for(int i = 0; i < NUM_LEDS-1; i++) {
+				leds[i] = CHSV(thunderColor, 255, 100 - (100/NUM_LEDS)*i);
+			}
+			FastLED.show();
+			delay(20);
+			// glow flash off
+			for(int i = 0; i < NUM_LEDS-1; i++) {
+				leds[i] = CHSV(thunderColor, 255, 0);
+			}
+			FastLED.show();
+			delay(20);
+		}
+	}
+
+	// if the thunder headed up
+	// draw the brigtheest LED at top
+	// dim the LED's as you go down
+	else {
+		// repeat flash 3 times
+		for (int i = 0; i < 3; i++) {
+			// glow flash on
+			for(int i = NUM_LEDS-1; i >= 0; i--) {
+				leds[i] = CHSV(thunderColor, 255, 100 - (100/NUM_LEDS)*i);
+			}
+			FastLED.show();
+			delay(20);
+			// glow flash off
+			for(int i = NUM_LEDS-1; i >= 0; i--) {
+				leds[i] = CHSV(thunderColor, 255, 0);
+			}
+			FastLED.show();
+			delay(20);
+		}
+	}
+
+	delay(thunderDelay);
 }
 
 void unicornPurpleRain(){
@@ -104,7 +222,6 @@ void unicornPurpleRain(){
 			}
 			dropBounces ++;
 		}
-
 
 		// flow and glow then fade out
 		// like water on a cold fullmoon night...
