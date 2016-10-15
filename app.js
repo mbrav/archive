091d6b4aee762cc9ffecaf973644ef276aa142ba
mbrav <mbrav@protonmail.com>
2016-10-15 01:25:26 -0400

    A realm of data personalities, in color

--- app.js
+++ app.js
@@ -17,38 +17,63 @@ console.log('SERVER STARTED');
 var socketList = {};
 
 var io = require('socket.io')(serv,{});
-io.sockets.on ('connection', function(socket) {
-	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
-	socket.z = 0;
-	socketList[socket.id] = socket;
-
+io.sockets.on('connection', function(socket) {
 	console.log('SOCKET CONNEXION');
 
-	socket.on('data', function(data){
-		console.log(data);
-		socket.emit('NSAmsg', {
-			msg:'YOU WHERE IDENTIFIED AS ' + data.user + ' BY THE NSA'
-		});
+	// setup the clinet once his settings are received
+	socket.on('settings', function(clientSettings) {
+		console.log('Client Settings Received', clientSettings);
+
+		var id = Math.random();
+		socket.width = clientSettings.width;
+		socket.height = clientSettings.height;
+		socket.id = id;
+		socket.name = "" + Math.floor(1000000 * id); // convert id into a name
+		socket.x = 0;
+		socket.y = clientSettings.fontSize;
+		socket.z = 0;
+		socket.color = clientSettings.color;
+		socket.fontSize = clientSettings.fontSize;
+		socketList[socket.id] = socket;
+  });
+
+	// disconnect client when he leaves
+	socket.on('disconnect', function() {
+		delete socketList[socket.id]
 	});
 });
 
 // set refresh rate
-var fps = 0.5;
+var fps = 30;
 setInterval(function(){
+
+	// data about all other players
+	var pack = [];
+
 	for (var i in socketList) {
 		var socket = socketList[i];
-		socket.x += 0.5;
-		if (socket.x > 300) {
+		socket.x += 10;
+
+		// limit to the canvas
+		if (socket.x > socket.width) {
 			socket.x = 0;
-			socket.y ++;
+			socket.y += 10;
+			if (socket.y > socket.height) {
+				socket.y = 0;
+				socket.z ++;
+			}
 		}
-		socket.z ++;
-		socket.emit('newPosition',{
+		pack.push({
 			x:socket.x,
 			y:socket.y,
-			z:socket.z
+			z:socket.z,
+			name:socket.name,
+			color: socket.color,
+			fontSize: socket.fontSize
 		});
 	}
-}), 1000/fps;
+	for (var i in socketList) {
+		// send info to every player about every other player
+		socket.emit('newPositions',pack);
+	}
+}, 1000/fps);
