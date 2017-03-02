185271f7ed02bc5691804ad728c5dce45c6874f4
mbrav <mbrav@protonmail.com>
2017-03-01 21:03:55 -0500

    wiki mathematicians

--- 06/node-scrape/app.js
+++ 06/node-scrape/app.js
@@ -1,13 +1,11 @@
 var cheerio = require('cheerio');
 var request = require('request');
-var sleep = require('sleep');
 
 var exec = require('child_process').exec;
 
 wikiFaces();
 // momaPS1();
 // parsonStudentWork();
-
 
 function scrape(l) {
   request({
@@ -19,50 +17,6 @@ function scrape(l) {
     console.log(body);
   });
 }
-
-function test(t) {
-  console.log(t);
-}
-
-// function wikiFaces() {
-//   var baseURL = 'https://en.wikipedia.org';
-//
-//   var first = "A",last = "Z";
-//
-//   for (var i = first.charCodeAt(0); i <= last.charCodeAt(0); i++) {
-//     var letter = String.fromCharCode(i);
-//
-//     request({
-//       method: 'GET',
-//       url: baseURL + '/wiki/List_of_mathematicians_(' + letter + ')'
-//     }, function(err, response, body) {
-//       if (err) return console.error(err);
-//
-//       // console.log(body);
-//
-//       var $ = cheerio.load(body);
-//
-//       $('div.mw-content-ltr ul li').each(function() {
-//
-//         // person link
-//         var a = $('a', this);
-//         var l = baseURL + a.attr('href');
-//         console.log('requesting: ' + l);
-//
-//
-//         request('https://en.wikipedia.org', function(err, res, h) {
-//           test("HI!!!!");
-//           if (!err && res.statusCode == 200) {
-//             console.log(h);
-//             var $ = cheerio.load(h);
-//           }
-//         });
-//
-//       });
-//
-//     });
-//   }
-// }
 
 // scraping wikiFaces past exibitions
 function wikiFaces() {
@@ -95,15 +49,15 @@ function wikiFaces() {
         request({
           method: 'GET',
           url: l
-        }, function(err, response, body) {
-          // if (err) return console.error(err);
+        }, function(err, response, body2) {
+          if (err) return console.error(err);
 
-          var $ = cheerio.load(body);
+          var $2 = cheerio.load(body2);
 
-          var img = $('table.infobox img', body);
-          var title = $('h1.firstHeading', body);
+          var img = $2('table.infobox img', body2);
+          var title = $2('h1.firstHeading', body2);
 
-          var command = 'wget "' + img.attr('src') + '" -P ./wiki-faces-scrapes';
+          var command = 'wget "http:' + img.attr('src') + '" -P ./wiki-faces-scrapes';
 
           if (img.attr('src') != null && img.attr('src') != undefined) {
             console.log(command);
@@ -172,6 +126,8 @@ function momaPS1() {
 
 // scraping Parsons Student Work/
 function parsonStudentWork() {
+
+  var baseURL = 'http://www.newschool.edu'
   request({
     method: 'GET',
     url: 'http://www.newschool.edu/parsons/student-work/'
@@ -180,26 +136,25 @@ function parsonStudentWork() {
 
     $ = cheerio.load(body);
 
-    // $('div.small-up-1').each(function() {
-    //   var href = $('a', this).attr('href');
-    //   if (href.lastIndexOf('/') > 0) {
-    //     console.log($('h3', this).text()); // student name
-    //   }
-    // });
-    // $('div.column').each(function() {
-    //   var img = $('h3');
-    //   console.log(img.text());
-    // });
-
     $('div.column').each(function() {
-      var img = $('img', this).attr('src');
+      var img = $('img', this);
       var majorName = $('p.profile-program-name', this).text();
 
-      // console.log('![](http://www.newschool.edu' + img + ')'); // markdown images
-      // console.log('http://www.newschool.edu' + img + ' , ' + majorName); // csv output
-      console.log('http://www.newschool.edu' + img); // just links
+      console.log('http://www.newschool.edu' + img.attr('src')); // just links
+
+      var command = 'wget "' + baseURL + img.attr('src') + '" -P ./parsons-img-scrapes';
+
+      console.log(command);
+      if (img.attr('src') != null) {
+        exec(command, (err, stdout, stderr) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(stdout);
+        });
+      }
 
     });
-
   });
 }
