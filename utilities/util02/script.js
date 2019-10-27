69bafa4eb2f8f088048f443b6f62459309723e72
mbrav <mbrav@protonmail.com>
2019-10-27 19:32:33 -0400

    gathering geolocation...

--- utilities/util02/script.js
+++ utilities/util02/script.js
@@ -2,111 +2,108 @@ var geocoder;
 var map;
 
 // specify data variable based on the .js file included in the .html
-var data = nycDomainRegistrations.data;
+var data = evictions;
 
 // new object storage
 var storage = [];
+
+// keep count, useful once requests become exausted
 var count = 0;
+var start_count = count;
 
 init();
 
 function init() {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(40.88, -73.82);
-  var mapOptions = {
-    zoom: 13,
-    center: latlng
-  }
 
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  objectSaveHackInit(); // the big boy
-  getDataInfo();
-  startGeocoding(); // another big boy
+	objectSaveHackInit(); // the big boy
+	getDataInfo();
+	startGeocoding(); // another big boy
 }
 
 function getDataInfo() {
-  console.log("Length of Address Data: " + data.length);
-  for (var i = 0; i < data[3].length; i++) {
-    console.log(i, data[0][i]);
-  }
+	console.log("Data Length: " + data.length);
 }
 
 function startGeocoding() {
-  setInterval(function(){
-    if (count < data.length) {
-      // var address = data[count][12] + ", " + data[count][16] + ", " + "NY";
-      var address = data[count][8];
-      console.log("GEOCODE REQUEST: ", address);
+	setInterval(function() {
+		if (count < data.length) {
 
-      // extra data to include in object storage
-      var additionalData = {
-        count : parseInt(data[count][10])
-      };
+      var request = data[count];
 
-      // request address to geocode (and include additionalData in output)
-      codeAddress(address, additionalData);
-    }
-    if (count == data.length) {
-      console.log(storage);
-      // save result to a file
-      console.save(storage, "geocoded-data.js");
-    }
-    count++;
-  },2000);
-}
+      console.log("GEOCODE REQUEST: ", request);
 
-// geocode address using Google Maps API
-function codeAddress(address, additionalData) {
-  geocoder.geocode({
-    'address': address
-  }, function(results, status) {
-    if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": encodeURI("https://us1.locationiq.com/v1/search.php?key=pk.3b4a08769fbd2a9d62d5844fd1539ca2&q="
+        + request.address
+        + ", "
+        + request.zip
+        + ", "
+        + request.borough
+        + ", NY, USA"
+        + "&format=json"),
+        "method": "GET"
+      }
+
+      $.ajax(settings).done(function(response) {
+        console.log("GEOCODE RESPONSE: ", response);
+        var output = {
+          lon: response[0].lon,
+          lat: response[0].lat,
+          date: data[count].date,
+          first_name: data[count].first_name,
+          last_name: data[count].last_name
+        };
+
+        storage.push(output);
+
+        count++;
       });
-      console.log(count, "RESPONSE - LAT: ", results[0].geometry.location.lat(), "LNG: ", results[0].geometry.location.lng());
-      var output = {
-        location: results[0].geometry.location,
-        info: additionalData
-      };
-      storage.push(output);
-    } else {
-      console.log('ERROR: Geocode was not successful for the following reason: ' + status);
-    }
-  });
+
+
+		}
+
+    // save file once it is finished
+		if (count == data.length) {
+			console.log(storage);
+			// save result to a file
+			console.save(storage, "geocoded-data.js");
+		}
+	}, 2000);
 }
+
 
 // HACK: console save file function
 // allows to save an object file
 // console.save(yourObject, "yourFileName.js");
 function objectSaveHackInit() {
-  (function(console){
+	(function(console) {
 
-    console.save = function(data, filename){
+		console.save = function(data, filename) {
 
-      if(!data) {
-        console.error('Console.save: No data')
-        return;
-      }
+			if (!data) {
+				console.error('Console.save: No data')
+				return;
+			}
 
-      if(!filename) filename = 'console.json'
+			if (!filename) filename = 'console.json'
 
-      if(typeof data === "object"){
-        data = JSON.stringify(data, undefined, 4)
-      }
+			if (typeof data === "object") {
+				data = JSON.stringify(data, undefined, 4)
+			}
 
-      var blob = new Blob([data], {type: 'text/json'}),
-      e    = document.createEvent('MouseEvents'),
-      a    = document.createElement('a')
+			var blob = new Blob([data], {
+					type: 'text/json'
+				}),
+				e = document.createEvent('MouseEvents'),
+				a = document.createElement('a')
 
-      a.download = filename
-      a.href = window.URL.createObjectURL(blob)
-      a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
-      e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-      a.dispatchEvent(e)
-    }
-  })(console)
+			a.download = filename
+			a.href = window.URL.createObjectURL(blob)
+			a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+			e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+			a.dispatchEvent(e)
+		}
+	})(console)
 }
