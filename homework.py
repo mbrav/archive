0ac2576a1f0378df22d9d7411c6b553d6a1235e0
mbrav <mbrav@protonmail.com>
2021-07-07 20:21:37 +0300

    __name__ fix

--- homework.py
+++ homework.py
@@ -163,19 +163,25 @@ class CashCalculator(Calculator):
                     f' - {rate:.2f} {cur_format}')
 
 
-cash_calculator = CashCalculator(1000)
+if __name__ == "__main__":
+    """Исполняемый код лучше выносить в верхний уровень.
+    Чтобы при импорте из текущего модуля не запускалось всё, что здесь описано
+    https://docs.python.org/3/library/__main__.html
+    """
 
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='08.11.2019'))
+    cash_calculator = CashCalculator(1000)
 
-print(cash_calculator.get_today_cash_remained('rub'))
-# должно напечататься
-# На сегодня осталось 555 руб
+    # дата в параметрах не указана,
+    # так что по умолчанию к записи
+    # должна автоматически добавиться сегодняшняя дата
+    cash_calculator.add_record(Record(amount=145, comment='кофе'))
+    # и к этой записи тоже дата должна добавиться автоматически
+    cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+    # а тут пользователь указал дату, сохраняем её
+    cash_calculator.add_record(Record(amount=3000,
+                                      comment='бар в Танин др',
+                                      date='08.11.2019'))
+
+    print(cash_calculator.get_today_cash_remained('rub'))
+    # должно напечататься
+    # На сегодня осталось 555 руб
