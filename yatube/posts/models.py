7e306bcf081cf2c91b3b3ac39de7faca48b37e2e
mbrav <mbrav@protonmail.com>
2021-07-15 21:37:18 +0300

    views config

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -8,8 +8,8 @@ User = get_user_model()
 
 class Group(models.Model):
 
-    title = models.CharField(max_length=255, unique=False, blank=False,)
-    slug = models.CharField(max_length=255,)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(blank=True,)
 
     class Meta:
@@ -22,7 +22,7 @@ class Group(models.Model):
 
 class Post(models.Model):
 
-    text = models.TextField(blank=True,)
+    text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True,)
     author = models.ForeignKey(
         User,
@@ -32,6 +32,8 @@ class Post(models.Model):
 
     group = models.ForeignKey(
         Group,
+        null=True,
+        blank=True,
         on_delete=models.CASCADE,
         related_name="posts"
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    content = "Home"
+    html = "<html><body>%s</body></html>" % content
+    return HttpResponse(html)
+
+
+def group_posts(request, group_slug):
+    content = group_slug
+    html = "<html><body>Group: %s</body></html>" % content
+    return HttpResponse(html)
