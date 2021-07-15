f4eafd4159d1323444061039007f3122dcb99887
mbrav <mbrav@protonmail.com>
2021-07-16 01:54:30 +0300

    static add

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Group, Post
+
 # Create your views here.
 
 
 def index(request):
-    content = "Home"
-    html = "<html><body>%s</body></html>" % content
-    return HttpResponse(html)
+    latest_posts = Post.objects.order_by('-pub_date')[:10]
+    return render(request, 'index.html', {'title': 'Δом', 'posts': latest_posts})
 
 
 def group_posts(request, group_slug):
