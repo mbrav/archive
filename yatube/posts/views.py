1e7940fdaa1bf57539371f80579fcf73232cb820
mbrav <mbrav@protonmail.com>
2021-08-15 16:08:37 +0300

    added paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,32 +1,58 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from .models import User, Post, Group
 
 
 def index(request):
-    return render(request, 'posts/index.html')
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    context = {
+        'page': page,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def profile(request, username):
-    return render(
-        request,
-        'posts/profile.html',
-        # {'profile': profile}
-    )
+    post_list = Post.objects.filter(author__username=username)
+    profile = User.objects.get(username=username)
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    context = {
+        'page': page,
+        'profile': profile,
+    }
+    return render(request, 'posts/profile.html', context)
 
 
-def group_list(request, slug):
-    return render(
-        request,
-        'posts/group_list.html',
-        # {'group': group}
-    )
+def group_list(request, group_slug):
+    post_list = Post.objects.filter(group__slug=group_slug)
+    group = Group.objects.get(slug=group_slug)
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    context = {
+        'page': page,
+        'group': group,
+    }
+    return render(request, 'posts/group_list.html', context)
 
 
 def post(request, post_id):
-    return render(
-        request,
-        'posts/post_detail.html',
-        # {'post': post}
-    )
+    post = Post.objects.get(id=post_id)
+
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/post_detail.html', context)
 
 
 def create_post(request):
