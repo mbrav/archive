5f611cb3e672907f79f5245f69372866df006ded
mbrav <mbrav@protonmail.com>
2021-08-17 16:16:07 +0300

    auth config

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,14 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import forms
 from django.urls import reverse_lazy
-from django.shortcuts import render
 from django.views import generic
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('users:login')
+    form_class = forms.UserCreationForm
+    success_url = reverse_lazy('index')
     template_name = 'users/signup.html'
-
-
-def logout(request):
-    return render(request, 'users/logged_out.html')
