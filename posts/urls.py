8290f20f1e76d804c3e7784efd922eee0d2e09c7
mbrav <mbrav@protonmail.com>
2021-10-02 15:51:42 +0300

    7.View-классы API - Урок 3

--- posts/urls.py
+++ posts/urls.py
@@ -2,9 +2,9 @@
 
 from django.urls import include, path
 
-from .views import APIPost, APIPostDetail
+from .views import APIPostList, APIPostDetail
 
 urlpatterns = [
-    path('api/v1/posts/', APIPost.as_view()),
+    path('api/v1/posts/', APIPostList.as_view()),
     path('api/v1/posts/<int:pk>/', APIPostDetail.as_view()),
 ] 
--- posts/views.py
+++ posts/views.py
@@ -8,42 +8,11 @@ from .models import Post
 from .serializers import PostSerializer
 
 
-class APIPost(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class APIPostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class APIPostDetail(APIView):
-    def get(self, request, pk):
-        post = Post.objects.get(pk=pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def put(self, request, pk):
-        post = Post.objects.get(pk=pk)
-        serializer = PostSerializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, pk):
-        post = Post.objects.get(pk=pk)
-        serializer = PostSerializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        post = Post.objects.get(pk=pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class APIPostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
