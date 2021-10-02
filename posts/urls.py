e71d17ae8501969d3d8e0819397dfd9771d3bff8
mbrav <mbrav@protonmail.com>
2021-10-02 17:57:06 +0300

    8.Вьюсеты и роутеры - Задача 1

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,14 @@
 #  Импортируйте в код всё необходимое
 
+from rest_framework.routers import SimpleRouter
+
 from django.urls import include, path
 
-from .views import APIPostList, APIPostDetail
+from .views import PostViewSet
+
+router = SimpleRouter()
+router.register('api/v1/posts', PostViewSet)
 
 urlpatterns = [
-    path('api/v1/posts/', APIPostList.as_view()),
-    path('api/v1/posts/<int:pk>/', APIPostDetail.as_view()),
+    path('', include(router.urls)),
 ] 
