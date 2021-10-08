c96006e2733f6c87cabc5b716205a3416e6fd92a
mbrav <mbrav@protonmail.com>
2021-10-08 21:52:52 +0300

    13.Аутентификация по токену. JWT + Djoser 1/1

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Post, Group
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Group)
+admin.site.register(Comment)
+admin.site.register(Follow)
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,24 +1,14 @@
 from rest_framework import serializers
-
 from .models import Post
 
 
-class CountLen(serializers.Field):
-    def to_count_len(self, text):
-        try:
-            data = len(text)
-        except ValueError:
-            raise serializers.ValidationError('Для этого цвета нет имени')
-        return data
-
-
 class PostSerializer(serializers.ModelSerializer):
-    character_quantity = serializers.SerializerMethodField()
-
     class Meta:
-        fields = ('id', 'text', 'author', 'image',
-                  'pub_date', 'character_quantity')
+        fields = ('id', 'text', 'author', 'image', 'pub_date')
+        read_only_fields = ('author',)
         model = Post
-
-    def get_character_quantity(self, obj):
-        return len(obj.text)
+        def get_post(self, request, pk=None):
+            queryset = Post.objects.all()
+            post = get_object_or_404(queryset, pk=pk)
+            serializer = CatSerializer(post)
+            return Response(serializer.data) 
